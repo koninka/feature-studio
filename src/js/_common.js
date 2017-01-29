ccdf65ccd838c09f4001b741fbc4967abd8520ae
whats0n <mrdvv@mail.ru>
2017-01-29 18:34:20 +0200

    update

--- src/js/_common.js
+++ src/js/_common.js
@@ -1,9 +1,11 @@
 import {BODY, touch} from './_constants';
 import {NAV} from './components/_nav';
 import MAP from './components/_map';
+import VALIDATION from './components/_validation';
 
 $(document).ready(e => {
 	if (!touch()) BODY.addClass('no-touch');
 	NAV.init();
 	MAP();
+	VALIDATION();
 });
