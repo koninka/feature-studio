2ac4b9232c3051c6b3e6dc3f3e2539cc499c419f
Валерий <kaktuz34@gmail.com>
2017-01-29 21:20:05 +0200

    added modal-slider

--- src/js/components/_modal-slider.js
+++ src/js/components/_modal-slider.js
@@ -3,6 +3,23 @@ import slick from 'slick-carousel';
 const SLICKMODAL = () => {
 	let sliderFor = $('.js-modal-slider-for'),
 		sliderNav = $('.js-modal-slider-nav');
+
+	sliderFor.slick({
+		lazyLoad: 'ondemand',
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.js-modal-slider-nav'
+	});
+	sliderNav.slick({
+		lazyLoad: 'ondemand',
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		asNavFor: '.js-modal-slider-for',
+		dots: false,
+		focusOnSelect: true
+	});
 };
 
 export default SLICKMODAL;
