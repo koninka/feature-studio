16c4fe4a8560decb7d349d819ec69aeb12b93f16
Валерий <kaktuz34@gmail.com>
2017-01-30 01:51:49 +0200

    counter from modal

--- src/js/_common.js
+++ src/js/_common.js
@@ -2,12 +2,11 @@ import {BODY, touch} from './_constants';
 import {NAV} from './components/_nav';
 import MAP from './components/_map';
 import VALIDATION from './components/_validation';
-import SLICKMODAL from './components/_modal-slider';
+import './components/_modal-slider';
 
 $(document).ready(e => {
 	if (!touch()) BODY.addClass('no-touch');
 	NAV.init();
 	MAP();
 	VALIDATION();
-	SLICKMODAL();
 });
--- src/js/components/_modal-slider.js
+++ src/js/components/_modal-slider.js
@@ -1,39 +1,91 @@
 import slick from 'slick-carousel';
-
-const SLICKMODAL = () => {
-	let sliderFor = $('.js-modal-slider-for'),
-		sliderNav = $('.js-modal-slider-nav');
-
-	sliderFor.slick({
-		lazyLoad: 'ondemand',
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: false,
-		fade: true,
-		asNavFor: '.js-modal-slider-nav'
-	});
-	sliderNav.slick({
-		lazyLoad: 'ondemand',
-		slidesToShow: 5,
-		slidesToScroll: 1,
-		asNavFor: '.js-modal-slider-for',
-		dots: false,
-		focusOnSelect: true
-	});
-};
-
-export default SLICKMODAL;
-
-// export default class Modal {
-// constructor(config) {
-// 		this._wrapper = config.wrapper;
-// 		this._element = this._wrapper.querySelector(config.element);
-// 		this._activeName = config.activeName;
-// 		this._init();
-// 	}
-
-// 	_init() {
-// 		this._createController();
-// 		this._createScene();
-// 	}
+	
+// 	sliderNav.slick({
+// 		lazyLoad: 'ondemand',
+// 		slidesToShow: 5,
+// 		slidesToScroll: 1,
+// 		asNavFor: '.js-modal-slider-for',
+// 		dots: false,
+// 		focusOnSelect: true
+// 	});
 // };
+
+
+;(function(){
+	let productItem = $('.js-product'),
+		productLink = $('.js-product-link');
+	productLink.on('click', function() {
+		let _this = $(this);
+		productLink.removeClass('is-active');
+		_this.addClass('is-active');
+		MODAL(_this);
+		return false;
+	});
+})();
+
+const MODAL = (link) => {
+	let product = $('.js-product'),
+		parent = link.parent('.js-product'),
+		parents = link.parents('.js-product-list'),
+		name = link.data('name'),
+		modals = $('.js-modal'),
+		modalWrap = $('.js-modal-wrap'),
+		activeModal = $("[data-modal="+name+"]"),
+		sliderFor = activeModal.find('.js-modal-slider-for'),
+		sliderNav = activeModal.find('.js-modal-slider-nav'),
+		windowWidth = $(window).width(),
+		parentPrevAll = parent.prevAll().length,
+		rowCounter,
+		row;
+	
+	if (!sliderFor.hasClass('slick-initialized')) {
+		sliderFor.slick({
+			lazyLoad: 'ondemand',
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: false,
+			fade: true,
+			asNavFor: '.js-modal-slider-nav'
+		});
+	}
+	
+	if (!sliderNav.hasClass('slick-initialized')) {
+		sliderNav.slick({
+			lazyLoad: 'ondemand',
+			slidesToShow: 5,
+			slidesToScroll: 1,
+			asNavFor: '.js-modal-slider-for',
+			dots: false,
+			focusOnSelect: true
+		});
+	}
+	
+	function whole(num) {
+		return (num ^ 0) === num;
+	}
+
+	if (windowWidth > 900) {
+		rowCounter = 5;
+	}
+	else {
+		rowCounter = 3;
+	};
+
+	row = parentPrevAll/rowCounter;
+	
+	if (whole(row)) {
+		if (row == 0) {
+			row = 0;
+		}
+	}
+	else {
+		row = Math.floor(row);
+	};
+
+	row = row*rowCounter+rowCounter;
+	modalWrap.remove();
+
+	console.log(row-1);
+	// parents.find('.js-product').eq(row-1).after('<div class="js-modal-wrap"></div>');
+	// activeModal.appendTo('.js-modal-wrap');
+};
