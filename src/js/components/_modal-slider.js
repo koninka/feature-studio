6bb28a13438db072c67b68c3b38baa472b0b70ad
Валерий <kaktuz34@gmail.com>
2017-01-30 22:48:55 +0200

    finish products modal

--- src/js/components/_modal-slider.js
+++ src/js/components/_modal-slider.js
@@ -1,59 +1,37 @@
+import { ACTIVE } from './../_constants';
 import slick from 'slick-carousel';
-	
-// 	sliderNav.slick({
-// 		lazyLoad: 'ondemand',
-// 		slidesToShow: 5,
-// 		slidesToScroll: 1,
-// 		asNavFor: '.js-modal-slider-for',
-// 		dots: false,
-// 		focusOnSelect: true
-// 	});
-// };
-
 
 ;(function(){
-	let productItem = $('.js-product'),
-		productLink = $('.js-product-link');
+
+	let productLink = $('.js-product-link');
+
 	productLink.on('click', function() {
-		let _this = $(this);
-		productLink.removeClass('is-active');
-		_this.addClass('is-active');
-		MODAL(_this);
+		MODAL($(this));
 		return false;
 	});
+
 })();
 
 const MODAL = (link) => {
 	let product = $('.js-product'),
 		parent = link.parent('.js-product'),
 		parents = link.parents('.js-product-list'),
+		item = parents.find('.js-product'),
+		productLink = $('.js-product-link'),
+
 		name = link.data('name'),
 		modals = $('.js-modal'),
 		modalWrap = $('.js-modal-wrap'),
-		activeModal = $("[data-modal="+name+"]"),
-		sliderFor = activeModal.find('.js-modal-slider-for'),
-		sliderNav = activeModal.find('.js-modal-slider-nav'),
+		modal = $("[data-modal="+name+"]"),
+		openModal = $(".js-product-list .js-modal"),
+		delay = 300,
+		
 		windowWidth = $(window).width(),
-		productPrevAll = parent.prevAll().length;
+		productPrevAll = parent.prevAll().length,
+
+		slickFor = '.js-modal-slider-for',
+		slickNav = '.js-modal-slider-nav';
 	
-	if (!sliderFor.hasClass('slick-initialized') && !sliderNav.hasClass('slick-initialized')) {
-		sliderFor.slick({
-			lazyLoad: 'ondemand',
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			arrows: false,
-			fade: true,
-			asNavFor: '.js-modal-slider-nav'
-		});
-		sliderNav.slick({
-			lazyLoad: 'ondemand',
-			slidesToShow: 5,
-			slidesToScroll: 1,
-			asNavFor: '.js-modal-slider-for',
-			dots: false,
-			focusOnSelect: true
-		});
-	};
 	
 	let counter = () => {
 		if (windowWidth > 900) {
@@ -68,9 +46,81 @@ const MODAL = (link) => {
 		return ( Math.floor( productPrevAll/counter() + 1 ) * counter() ) - 1;
 	};
 
-	console.log(Math.floor( productPrevAll/counter() ) * counter(), getIndex());
+	let createModal = () => {
+		let wrapper = '.js-modal-wrap',
+			tamplate = '<div class="js-modal-wrap"></div>';
 
-	// modalWrap.remove();
-	// parents.find('.js-product').eq(getIndex()).after('<div class="js-modal-wrap"></div>');
-	// activeModal.appendTo('.js-modal-wrap');
+		modalWrap.remove();
+		item.eq( getIndex() ).after(tamplate);
+
+		if (openModal.length) {
+			modal.clone().appendTo(wrapper).show();
+		}
+		else {
+			modal.clone().appendTo(wrapper).slideDown(delay);
+		}
+
+	}
+
+	let hideModal = () => {
+		
+		openModal.slideUp(delay);
+		setTimeout(() => {
+			modalWrap.remove();
+		}, delay);
+	};
+
+	let createActiveItem = () => {
+		
+		if (link.hasClass(ACTIVE)) {
+			productLink.removeClass(ACTIVE);
+			hideModal();
+		}
+		else {
+			productLink.removeClass(ACTIVE);
+			link.addClass(ACTIVE);
+			createModal();
+		}
+	}
+	createActiveItem();
+
+	let initSlider = () => {
+		let activeModal = parents.find("[data-modal="+name+"]"),
+			sliderFor = activeModal.find(slickFor),
+			sliderNav = activeModal.find(slickNav),
+			slickActive = 'slick-initialized',
+			loaded = 'is-loaded',
+
+			sliderForOptions = {
+				lazyLoad: 'ondemand',
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: false,
+				fade: true,
+				asNavFor: slickNav
+			},
+
+			sliderNavOptions = {
+				lazyLoad: 'ondemand',
+				slidesToShow: 5,
+				slidesToScroll: 1,
+				asNavFor: slickFor,
+				dots: false,
+				focusOnSelect: true
+			};
+
+		sliderFor.on('init', function() {
+			sliderFor.addClass(loaded);
+		});
+		sliderNav.on('init', function() {
+			sliderNav.addClass(loaded);
+		});
+
+		if ( !sliderFor.hasClass(slickActive) && !sliderNav.hasClass(slickActive) ) {
+			sliderFor.slick(sliderForOptions);
+			sliderNav.slick(sliderNavOptions);
+		};
+	};
+	initSlider();
+	
 };
