b364dad2fe4e024f3b151c182e4948a45b1eb1b8
blb <blb@factorov.ru>
2017-04-13 23:54:30 +1000

    Test compile

--- build/js/form.js
+++ build/js/form.js
@@ -1,2 +1,50 @@
-!function(n){function r(o){if(e[o])return e[o].exports;var t=e[o]={exports:{},id:o,loaded:!1};return n[o].call(t.exports,t,t.exports,r),t.loaded=!0,t.exports}var e={};return r.m=n,r.c=e,r.p="js/",r(0)}([function(n,r){"use strict";window.handlers={},window.handlers.service=function(n,r){n()},window.handlers.price=function(n,r){n()}}]);
-//# sourceMappingURL=form.js.map
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "js/";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	eval("\"use strict\";\n\nwindow.handlers = {};\n\n// service form handlers\nwindow.handlers.service = function (successCallback, errorCallback) {\n    successCallback();\n};\n\n// service form handlers\nwindow.handlers.price = function (successCallback, errorCallback) {\n    successCallback();\n};\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMC5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy9zcmMvanMvZm9ybS5qcz8xZjA4Il0sInNvdXJjZXNDb250ZW50IjpbIndpbmRvdy5oYW5kbGVycyA9IHt9O1xuXG4vLyBzZXJ2aWNlIGZvcm0gaGFuZGxlcnNcbndpbmRvdy5oYW5kbGVycy5zZXJ2aWNlID0gZnVuY3Rpb24gKHN1Y2Nlc3NDYWxsYmFjaywgZXJyb3JDYWxsYmFjaykge1xuICAgIHN1Y2Nlc3NDYWxsYmFjaygpO1xufTtcblxuLy8gc2VydmljZSBmb3JtIGhhbmRsZXJzXG53aW5kb3cuaGFuZGxlcnMucHJpY2UgPSBmdW5jdGlvbiAoc3VjY2Vzc0NhbGxiYWNrLCBlcnJvckNhbGxiYWNrKSB7XG4gICAgc3VjY2Vzc0NhbGxiYWNrKCk7XG59O1xuXG5cblxuLy8gV0VCUEFDSyBGT09URVIgLy9cbi8vIHNyYy9qcy9mb3JtLmpzIl0sIm1hcHBpbmdzIjoiOztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBOyIsInNvdXJjZVJvb3QiOiIifQ==");
+
+/***/ }
+/******/ ]);
