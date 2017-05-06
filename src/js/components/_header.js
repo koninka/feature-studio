a545bc6faf9b3fa3b7c0fd255a39ea32eb58b86d
M. Tertishniy <mtertishniy@gmail.com>
2017-05-06 17:42:38 +1000

    Fix phone click (separated)

--- src/js/components/_header.js
+++ src/js/components/_header.js
@@ -2,8 +2,6 @@ import {WIN} from './../_constants';
 import './../lib/mask.min';
 
 export const HEADER = () => {
-  let headerVisible = $('.tel--hidden');
-  let headerHidden = $('.tel--visible');
   $('#phone_mask').mask("+7(999) 999-99-99");
   $('#phone_mask_modal').mask("+7(999) 999-99-99");
   $(document).mouseup(function (e) {
@@ -12,8 +10,9 @@ export const HEADER = () => {
         $(".modals").css('top', '-50%');
     }
   });
-  headerVisible.on('click', e => {
-    headerVisible.css('display', 'none');
-    headerHidden.css('display', 'block');
+  $('.tel--hidden').on('click', function () {
+    let $this = $(this);
+    $this.css('display', 'none');
+    $this.parent().find('.tel--visible').css('display', 'block');
   });
 };
