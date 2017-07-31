db46cf735fc88f90f83f634912191a17cfa3670b
Elena Bozhok <helenbozhok@yahoo.com>
2017-07-31 14:29:29 +1000

    Fix arrows of steps

--- src/js/components/_whyWe.js
+++ src/js/components/_whyWe.js
@@ -7,5 +7,6 @@ export const PHONE = () => {
 	$(document).on('click', '.phone__text--hidden', function () {
 		$(this).closest('.phone').find('.phone__text--hidden').fadeOut(300);
 		$(this).closest('.phone').find('.icon_arrow_up_holder').css('visibility', 'hidden');
-	});
+	});	
 };
+
